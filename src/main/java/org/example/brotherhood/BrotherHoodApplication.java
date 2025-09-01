e33b08649053c785da9daf32c4e3efba8eb940b8
김경민 <kkm07133@naver.com>
2025-09-01 16:44:30 +0900

    start

---
+++ src/main/java/org/example/brotherhood/BrotherHoodApplication.java
@@ -0,0 +1,13 @@
+package org.example.brotherhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherHoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrotherHoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/brotherhood/BrotherHoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.brotherhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrotherHoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
